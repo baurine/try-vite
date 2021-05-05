f49270f7fdc06f644f67b52d2868e8182f362511
baurine <2008.hbl@gmail.com>
2021-05-05 08:22:24 +0800

    sayHello

---
+++ vanilla/main.js
@@ -0,0 +1,5 @@
+import { nowTime } from './util.js'
+
+export function sayHello() {
+  window.alert(`hello, now the time is ${nowTime()}`)
+}
---
+++ vanilla/util.js
@@ -0,0 +1,3 @@
+export function nowTime() {
+  return Date().toString()
+}
